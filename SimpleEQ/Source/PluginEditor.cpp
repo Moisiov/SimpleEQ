93cdd397c0739852daac42f03b5e69fa780b68a8
Valto Moisio <valto.moisio@pilvit.fi>
2021-07-15 00:20:29 +0300

    Add basic slider graphics

--- SimpleEQ/Source/PluginEditor.cpp
+++ SimpleEQ/Source/PluginEditor.cpp
@@ -116,6 +116,36 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
     g.strokePath(responseCurve, PathStrokeType(2.f));
 }
 
+
+void RotarySliderWithLabels::paint(juce::Graphics& g)
+{
+    using namespace juce;
+
+    auto startAng = degreesToRadians(180.f + 45.f);
+    auto endAng = degreesToRadians(180.f - 45.f) + MathConstants<float>::twoPi;
+
+    auto range = getRange();
+
+    auto bounds = getSliderBounds();
+
+    getLookAndFeel().drawRotarySlider(
+        g,
+        bounds.getX(),
+        bounds.getY(),
+        bounds.getWidth(),
+        bounds.getHeight(),
+        jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0),
+        startAng,
+        endAng,
+        *this
+    );
+}
+
+juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
+{
+    return getLocalBounds();
+}
+
 //==============================================================================
 KarvaEQAudioProcessorEditor::KarvaEQAudioProcessorEditor(KarvaEQAudioProcessor& p)
     : AudioProcessorEditor(&p),
@@ -195,4 +225,46 @@ std::vector<juce::Component*> KarvaEQAudioProcessorEditor::getComps()
         &highCutSlopeSlider,
         &responseCurveComponent
     };
+}
+
+void LookAndFeel::drawRotarySlider(
+    juce::Graphics& g,
+    int x,
+    int y,
+    int width,
+    int height,
+    float sliderPosProportional,
+    float rotaryStartAngle,
+    float rotaryEndAngle,
+    juce::Slider&)
+{
+    using namespace juce;
+
+    auto bounds = Rectangle<float>(x, y, width, height);
+
+    // g.setColour(Colour(151u, 216u, 234u));
+    //g.fillEllipse(bounds);
+
+    g.setColour(Colour(97u, 202u, 197u));
+    g.drawEllipse(bounds, 1.f);
+
+    auto center = bounds.getCentre();
+
+    Path p;
+
+    Rectangle<float> r;
+    r.setLeft(center.getX() - 2);
+    r.setRight(center.getX() + 2);
+    r.setTop(bounds.getY());
+    r.setBottom(center.getY());
+
+    p.addRectangle(r);
+
+    jassert(rotaryStartAngle < rotaryEndAngle);
+
+    auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
+
+    p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+
+    g.fillPath(p);
 }
